8b6a5706c4e43ff95812fa110fff42d35548c8e4
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2023-08-10 16:18:47 +0300

    our child deleted

--- App.js
+++ App.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import CurrentWeather from './src/components/CurrentWeather'
 import UpcomingWeather from './src/components/UpcomingWeather'
-import OurChild from './src/components/OurChild'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <OurChild message={'hello'} />
-      <OurChild message={'greetings'} />
-      <OurChild message={'bye'} />
+      <UpcomingWeather />
     </View>
   )
 }
